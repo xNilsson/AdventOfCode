ac2c8367f7e238f22766f51f4abca70adb183d06
Christopher Nilsson <c@shapeapp.se>
2025-12-02 06:49:35 +0100

    optimized

--- 2025/02.py
+++ 2025/02.py
@@ -1,8 +1,11 @@
 print(chr(27)+'[2j')
 print('\033c')
-f = open('02.test', 'r')
 f = open('02.input', 'r')
+f = open('02.test', 'r')
 lines = [x.strip() for x in f.readlines()]
+
+from time import perf_counter
+t1 = perf_counter()
 
 def part1(x):
     has_found = False
@@ -11,13 +14,17 @@ def part1(x):
     return a == b
 
 def part2(x):
-    has_found = False
-    for i in range(1, len(x)//2 + 1):
-        unique = set()
-        for c in range(0, len(x), i):
-            chunk = x[c:c+i]
-            unique.add(chunk)
-        if(len(unique) == 1):
+    mid = len(x)//2
+    for idx in range(1, mid + 1):
+        prev = x[:idx]
+        equal = True
+        for c in range(0, len(x), idx):
+            chunk = x[c:c+idx]
+            if chunk != prev:
+                equal = False
+                break
+            prev = chunk
+        if equal:
             return True
     return False
 
@@ -37,7 +44,10 @@ for idrange in ranges.split(','):
         if part2(x):
             invalid_2.append(int(x))
 
+
 res1 = sum(invalid_1)
 res2 = sum(invalid_2)
 print('Part 1:', res1)
 print('Part 2:', res2)
+t2 = perf_counter()
+print('Time:', t2 - t1)
